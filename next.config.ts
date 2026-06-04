7f29f0a19e6c3d58dc2a331b097b992872f040c0
KebabObama <lucaschyba@gmail.com>
2026-06-04 12:20:55 +0200

    lint and navigation type safety

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
+  logging: {
+    browserToTerminal: true,
+    incomingRequests: true,
+    fetches: { fullUrl: true, hmrRefreshes: true },
+  },
   output: "export",
   images: { unoptimized: true },
-  basePath: '/chronicles'
-  // cacheComponents: true,
+  basePath: "/chronicles",
+  typedRoutes: true,
 };
 
 export default nextConfig;
--- src/components/layout/links.tsx
+++ src/components/layout/links.tsx
@@ -1,19 +1,27 @@
+import type { Route } from "next";
 import Link from "next/link";
+import type { IconType } from "react-icons";
 import { FaBalanceScale, FaHome } from "react-icons/fa";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { GiMagicSwirl, GiSwitchWeapon } from "react-icons/gi";
 
-const LINKS = [
-  { href: "/", label: "Domů", icon: FaHome },
-  { href: "/rules", label: "Pravidla", icon: FaBalanceScale },
-  { href: "/classes", label: "Postavy", icon: FaPeopleGroup },
-  { href: "/items", label: "Předměty", icon: GiSwitchWeapon },
-  { href: "/spells", label: "Schopnosti", icon: GiMagicSwirl },
+type LinkedUrl = {
+  href: Route;
+  label: string;
+  Icon: IconType;
+};
+
+const LINKS: LinkedUrl[] = [
+  { href: "/", label: "Domů", Icon: FaHome },
+  { href: "/rules", label: "Pravidla", Icon: FaBalanceScale },
+  { href: "/classes", label: "Postavy", Icon: FaPeopleGroup },
+  { href: "/items", label: "Předměty", Icon: GiSwitchWeapon },
+  { href: "/spells", label: "Schopnosti", Icon: GiMagicSwirl },
 ];
 
 export const Links = () => {
-  return LINKS.map(({ href, label, icon: Icon }) => (
-    <Link key={href} href={href} className="flex items-center gap-2 transition-colors hover:text-primary">
+  return LINKS.map(({ href, label, Icon }) => (
+    <Link key={label} href={href} className="flex items-center gap-2 transition-colors hover:text-primary">
       <Icon />
       {label}
     </Link>
