337470bd2ea3c816cfbd91dfb6d149b881eda666
KebabObama <lucaschyba@gmail.com>
2026-06-05 20:40:15 +0200

    FIX remove hook lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
-import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export type TriggerElement = React.ReactElement<React.ComponentPropsWithRef<"button">>;
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-};
-
-export const useDebounce = <T>(value: T, delay: number): T => {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => setDebouncedValue(value), delay);
-    return () => clearTimeout(handler);
-  }, [value, delay]);
-
-  return debouncedValue;
 };
