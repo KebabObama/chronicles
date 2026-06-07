d1722c887a8a06a9fdd777d007a39dc35c5b0f27
KebabObama <lucaschyba@gmail.com>
2026-06-07 13:42:19 +0200

    prep for custom domaim

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,16 @@ const prod = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
+  compiler: { removeConsole: prod },
+  output: "export",
+  images: { unoptimized: true },
+  basePath: process.env.BASE_PATH,
+  typedRoutes: true,
   logging: {
     browserToTerminal: !prod,
     incomingRequests: !prod,
     fetches: { fullUrl: !prod, hmrRefreshes: !prod },
   },
-  compiler: {
-    removeConsole: prod,
-    define: {
-      APP_NAME: process.env.APP_NAME as string,
-      BASE_PATH: process.env.BASE_PATH as string,
-    },
-  },
-  output: "export",
-  images: { unoptimized: true },
-  basePath: process.env.BASE_PATH,
-  typedRoutes: true,
 };
 
 export default nextConfig;
